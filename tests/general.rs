9a98c41ca1ec195703418921e2d1a167ea814baf
Tay754 <t7547347@gmail.com>
2024-08-13 06:53:34 -0400

    Delete tests directory
    Fraud
    
    Signed-off-by: Tay754 <t7547347@gmail.com>

--- tests/general.rs
+++
@@ -1,7 +0,0 @@
-use verso::verso_test;
-use winit::event_loop::EventLoopWindowTarget;
-
-fn smoke(_elwt: &EventLoopWindowTarget<()>) {}
-fn other_smoke(_elwt: &EventLoopWindowTarget<()>) {}
-
-verso_test!(smoke, other_smoke);
