365507c2258f4ebb804a21ddf9b949f90421ef7d
Wu Yuwei <Wu Yu Wei>
2024-08-17 13:51:35 +0900

    Add create method

--- src/rendering.rs
+++ src/rendering.rs
@@ -1,14 +1,24 @@
-#![deny(unsafe_code)]
-
-use std::cell::RefCell;
-use std::ffi::c_void;
+use std::ffi::{c_void, CStr};
 use std::rc::Rc;
+use std::{cell::RefCell, ffi::CString};
 
 use euclid::default::Size2D;
+use glutin::context::PossiblyCurrentContext;
+use glutin::surface::{Surface as GSurface, WindowSurface};
+use glutin::{
+    config::{Config, ConfigTemplateBuilder, GlConfig},
+    context::{ContextApi, ContextAttributesBuilder, Version},
+    display::GetGlDisplay,
+    prelude::{GlDisplay, NotCurrentGlContext},
+};
+use glutin_winit::{DisplayBuilder, GlWindow};
+use raw_window_handle::HasWindowHandle;
 use surfman::{
     Adapter, Connection, Context, ContextAttributeFlags, ContextAttributes, Device, Error, GLApi,
     GLVersion, NativeWidget, Surface, SurfaceAccess, SurfaceInfo, SurfaceType,
 };
+use winit::event_loop::ActiveEventLoop;
+use winit::window::Window;
 
 mod gl {
     #![allow(clippy::all)]
@@ -22,17 +32,92 @@ mod gl {
 #[derive(Clone)]
 pub struct RenderingContext(Rc<RenderingContextData>);
 
-struct RContext {}
+struct RContext {
+    context: PossiblyCurrentContext,
+    surface: GSurface<WindowSurface>,
+    gl: gl::Gl,
+}
 
 impl RContext {
-    // /// Create a rendering context instance.
-    // pub fn create(
-    //     connection: &Connection,
-    //     adapter: &Adapter,
-    //     surface_type: SurfaceType<NativeWidget>,
-    // ) -> Result<Self, Error> {
-    //     todo!()
-    // }
+    /// Create a rendering context instance.
+    pub fn create(
+        evl: &ActiveEventLoop,
+        window: &Window,
+    ) -> Result<Self, Box<dyn std::error::Error>> {
+        let template = ConfigTemplateBuilder::new()
+            .with_alpha_size(8)
+            .with_transparency(cfg!(macos));
+        let (_, gl_config) = DisplayBuilder::new().build(evl, template, gl_config_picker)?;
+
+        log::debug!("Picked a config with {} samples", gl_config.num_samples());
+
+        // XXX This will panic on Andoird, but we care about Deskyop for now.
+        let raw_window_handle = window.window_handle().ok().map(|handle| handle.as_raw());
+        // XXX The display could be obtained from any object created by it, so we can
+        // query it from the config.
+        let gl_display = gl_config.display();
+        // The context creation part.
+        let context_attributes = ContextAttributesBuilder::new().build(raw_window_handle);
+        // Since glutin by default tries to create OpenGL core context, which may not be
+        // present we should try gles.
+        let fallback_context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(ContextApi::Gles(None))
+            .build(raw_window_handle);
+        // There are also some old devices that support neither modern OpenGL nor GLES.
+        // To support these we can try and create a 2.1 context.
+        let legacy_context_attributes = ContextAttributesBuilder::new()
+            .with_context_api(ContextApi::OpenGl(Some(Version::new(2, 1))))
+            .build(raw_window_handle);
+        let not_current_gl_context = unsafe {
+            gl_display
+                .create_context(&gl_config, &context_attributes)
+                .unwrap_or_else(|_| {
+                    gl_display
+                        .create_context(&gl_config, &fallback_context_attributes)
+                        .unwrap_or_else(|_| {
+                            gl_display
+                                .create_context(&gl_config, &legacy_context_attributes)
+                                .expect("failed to create context")
+                        })
+                })
+        };
+
+        // Create surface
+        let attrs = window
+            .build_surface_attributes(Default::default())
+            .expect("Failed to build surface attributes");
+        let surface = unsafe {
+            gl_config
+                .display()
+                .create_window_surface(&gl_config, &attrs)
+                .unwrap()
+        };
+
+        // Make it current.
+        let context = not_current_gl_context.make_current(&surface).unwrap();
+
+        let gl = gl::Gl::load_with(|symbol| {
+            let symbol = CString::new(symbol).unwrap();
+            gl_display.get_proc_address(symbol.as_c_str()).cast()
+        });
+
+        if let Some(renderer) = get_gl_string(&gl, gl::RENDERER) {
+            log::debug!("Running on {}", renderer.to_string_lossy());
+        }
+        if let Some(version) = get_gl_string(&gl, gl::VERSION) {
+            log::debug!("OpenGL Version {}", version.to_string_lossy());
+        }
+
+        if let Some(shaders_version) = get_gl_string(&gl, gl::SHADING_LANGUAGE_VERSION) {
+            log::debug!("Shaders version on {}", shaders_version.to_string_lossy());
+        }
+
+        Ok(Self {
+            context,
+            surface,
+            gl,
+        })
+    }
 }
 
 struct RenderingContextData {
@@ -101,38 +186,12 @@ impl RenderingContext {
         device.destroy_surface(context, &mut surface)
     }
 
-    // /// Create a surface texture.
-    // pub fn create_surface_texture(&self, surface: Surface) -> Result<SurfaceTexture, Error> {
-    //     let device = &self.0.device.borrow();
-    //     let context = &mut self.0.context.borrow_mut();
-    //     device
-    //         .create_surface_texture(context, surface)
-    //         .map_err(|(error, _)| error)
-    // }
-    //
-    // /// Destroy a surface texture.
-    // pub fn destroy_surface_texture(
-    //     &self,
-    //     surface_texture: SurfaceTexture,
-    // ) -> Result<Surface, Error> {
-    //     let device = &self.0.device.borrow();
-    //     let context = &mut self.0.context.borrow_mut();
-    //     device
-    //         .destroy_surface_texture(context, surface_texture)
-    //         .map_err(|(error, _)| error)
-    // }
-
     /// Make GL context current.
     pub fn make_gl_context_current(&self) -> Result<(), Error> {
         let device = &self.0.device.borrow();
         let context = &self.0.context.borrow();
         device.make_context_current(context)
     }
-
-    // /// Get the swap chain of the rendering context.
-    // pub fn swap_chain(&self) -> Result<&SwapChain<Device>, Error> {
-    //     self.0.swap_chain.as_ref().ok_or(Error::WidgetAttached)
-    // }
 
     /// Resize the rendering context.
     pub fn resize(&self, size: Size2D<i32>) -> Result<(), Error> {
@@ -175,87 +234,46 @@ impl RenderingContext {
         device.bind_surface_to_context(context, surface).unwrap();
     }
 
-    // /// Get the device of the rendering context.
-    // pub fn device(&self) -> std::cell::Ref<Device> {
-    //     self.0.device.borrow()
-    // }
-
     /// Get the conntection of the rendering context.
     pub fn connection(&self) -> Connection {
         let device = &self.0.device.borrow();
         device.connection()
     }
 
-    // /// Get the adapter of the rendering context.
-    // pub fn adapter(&self) -> Adapter {
-    //     let device = &self.0.device.borrow();
-    //     device.adapter()
-    // }
-
-    // /// Get the native context of the rendering context.
-    // pub fn native_context(&self) -> NativeContext {
-    //     let device = &self.0.device.borrow();
-    //     let context = &self.0.context.borrow();
-    //     device.native_context(context)
-    // }
-
-    // pub fn native_device(&self) -> NativeDevice {
-    //     let device = &self.0.device.borrow();
-    //     device.native_device()
-    // }
-    //
-    // pub fn context_attributes(&self) -> ContextAttributes {
-    //     let device = &self.0.device.borrow();
-    //     let context = &self.0.context.borrow();
-    //     let descriptor = &device.context_descriptor(context);
-    //     device.context_descriptor_attributes(descriptor)
-    // }
-    //
     /// Get the context surface info of the rendering context.
     pub fn context_surface_info(&self) -> Result<Option<SurfaceInfo>, Error> {
         let device = &self.0.device.borrow();
         let context = &self.0.context.borrow();
         device.context_surface_info(context)
     }
-    //
-    // pub fn surface_info(&self, surface: &Surface) -> SurfaceInfo {
-    //     let device = &self.0.device.borrow();
-    //     device.surface_info(surface)
-    // }
-    //
-    // pub fn surface_texture_object(&self, surface: &SurfaceTexture) -> u32 {
-    //     let device = &self.0.device.borrow();
-    //     device.surface_texture_object(surface)
-    // }
-    //
     /// Get the proc address of the rendering context.
     pub fn get_proc_address(&self, name: &str) -> *const c_void {
         let device = &self.0.device.borrow();
         let context = &self.0.context.borrow();
         device.get_proc_address(context, name)
     }
-    //
-    // pub fn unbind_native_surface_from_context(&self) -> Result<(), Error> {
-    //     let device = self.0.device.borrow_mut();
-    //     let mut context = self.0.context.borrow_mut();
-    //     let mut surface = device.unbind_surface_from_context(&mut context)?.unwrap();
-    //     device.destroy_surface(&mut context, &mut surface)?;
-    //     Ok(())
-    // }
-    //
-    // pub fn bind_native_surface_to_context(&self, native_widget: NativeWidget) -> Result<(), Error> {
-    //     let mut device = self.0.device.borrow_mut();
-    //     let mut context = self.0.context.borrow_mut();
-    //     let surface_access = SurfaceAccess::GPUOnly;
-    //     let surface_type = SurfaceType::Widget { native_widget };
-    //     let surface = device.create_surface(&context, surface_access, surface_type)?;
-    //     device
-    //         .bind_surface_to_context(&mut context, surface)
-    //         .map_err(|(err, mut surface)| {
-    //             let _ = device.destroy_surface(&mut context, &mut surface);
-    //             err
-    //         })?;
-    //     device.make_context_current(&context)?;
-    //     Ok(())
-    // }
+}
+
+/// Find the config with the maximum number of samples, so our triangle will be
+/// smooth.
+pub fn gl_config_picker(configs: Box<dyn Iterator<Item = Config> + '_>) -> Config {
+    configs
+        .reduce(|accum, config| {
+            let transparency_check = config.supports_transparency().unwrap_or(false)
+                & !accum.supports_transparency().unwrap_or(false);
+
+            if transparency_check || config.num_samples() > accum.num_samples() {
+                config
+            } else {
+                accum
+            }
+        })
+        .unwrap()
+}
+
+fn get_gl_string(gl: &gl::Gl, variant: gl::types::GLenum) -> Option<&'static CStr> {
+    unsafe {
+        let s = gl.GetString(variant);
+        (!s.is_null()).then(|| CStr::from_ptr(s.cast()))
+    }
 }
