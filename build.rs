a60e5e455b95656c159cc5ef59988160c1aa00fc
Wu Yu Wei <yuweiwu@pm.me>
2024-08-18 20:55:51 +0900

    Remove unused dependencies

--- build.rs
+++ build.rs
@@ -1,7 +1,3 @@
-use std::{env, fs::File, path::PathBuf};
-
-use gl_generator::{Api, Fallbacks, Profile, Registry, StructGenerator};
-
 fn main() {
     cfg_aliases::cfg_aliases! {
         // Platforms
@@ -15,12 +11,4 @@ fn main() {
 
     #[cfg(all(feature = "packager", target_os = "macos"))]
     println!("cargo:rustc-link-arg=-Wl,-rpath,@executable_path/../Resources/lib");
-
-    // Generate GL bindings
-    let dest = PathBuf::from(&env::var("OUT_DIR").unwrap());
-    println!("cargo:rerun-if-changed=build.rs");
-    let mut file = File::create(dest.join("gl_bindings.rs")).unwrap();
-    Registry::new(Api::Gles2, (3, 0), Profile::Core, Fallbacks::All, [])
-        .write_bindings(StructGenerator, &mut file)
-        .unwrap();
 }
