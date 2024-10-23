44fd77feb72a322f1d8ed2059ba112f697b9af36
Tony <legendmastertony@gmail.com>
2024-10-23 15:21:49 +0800

    Move functions to a struct

--- verso/examples/simple.rs
+++ verso/examples/simple.rs
@@ -1,18 +1,14 @@
 use std::{path::PathBuf, thread::sleep, time::Duration};
-use versoview_messages::ControllerMessage;
 
 fn main() {
-    let sender = verso::run_versoview(
+    let controller = verso::VersoviewController::new(
         PathBuf::from("target/debug/versoview"),
         url::Url::parse("https://example.com").unwrap(),
     );
     sleep(Duration::from_secs(10));
-    println!("Sending NavigateTo https://docs.rs");
-    sender
-        .send(ControllerMessage::NavigateTo(
-            url::Url::parse("https://docs.rs").unwrap(),
-        ))
-        .unwrap();
+    dbg!(controller
+        .navigate(url::Url::parse("https://docs.rs").unwrap())
+        .unwrap());
     loop {
         sleep(Duration::from_secs(10));
     }
--- verso/src/lib.rs
+++ verso/src/lib.rs
@@ -3,17 +3,25 @@ use versoview_messages::ControllerMessage;
 
 use ipc_channel::ipc::{IpcOneShotServer, IpcSender};
 
-pub fn run_versoview(
-    verso_path: impl AsRef<Path>,
-    initial_url: url::Url,
-) -> IpcSender<ControllerMessage> {
-    let path = verso_path.as_ref();
-    let (server, server_name) = IpcOneShotServer::<IpcSender<ControllerMessage>>::new().unwrap();
-    Command::new(path)
-        .arg(format!("--ipc-channel={server_name}"))
-        .arg(format!("--url={initial_url}"))
-        .spawn()
-        .unwrap();
-    let (_, sender) = server.accept().unwrap();
-    sender
+pub struct VersoviewController(IpcSender<ControllerMessage>);
+
+impl VersoviewController {
+    /// Create a new verso instance and get the controller to it
+    pub fn new(verso_path: impl AsRef<Path>, initial_url: url::Url) -> Self {
+        let path = verso_path.as_ref();
+        let (server, server_name) =
+            IpcOneShotServer::<IpcSender<ControllerMessage>>::new().unwrap();
+        Command::new(path)
+            .arg(format!("--ipc-channel={server_name}"))
+            .arg(format!("--url={initial_url}"))
+            .spawn()
+            .unwrap();
+        let (_, sender) = server.accept().unwrap();
+        Self(sender)
+    }
+
+    /// Navigate to url
+    pub fn navigate(&self, url: url::Url) -> Result<(), Box<ipc_channel::ErrorKind>> {
+        self.0.send(ControllerMessage::NavigateTo(url))
+    }
 }
