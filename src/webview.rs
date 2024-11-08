3a79e84287dfc4b09955506ec6bff957b11b174f
Tony <legendmastertony@gmail.com>
2024-11-08 13:07:57 +0800

    Handle error instead of unwrap

--- src/webview.rs
+++ src/webview.rs
@@ -84,19 +84,13 @@ impl Window {
                 send_to_constellation(sender, ConstellationMsg::FocusWebView(webview_id));
             }
             EmbedderMsg::AllowNavigationRequest(id, url) => {
-                let mut allow = true;
-                if let Some(ref sender) =
-                    *self.event_listeners.on_navigation_starting.lock().unwrap()
-                {
-                    let (result_sender, receiver) = ipc::channel::<bool>().unwrap();
-                    if let Some(result) = sender
-                        .send((url.into_url(), result_sender))
-                        .ok()
-                        .and_then(|_| receiver.recv().ok())
-                    {
-                        allow = result
+                let allow = match self.allow_navigation(url) {
+                    Ok(allow) => allow,
+                    Err(e) => {
+                        log::error!("Error when calling navigation handler: {e}");
+                        true
                     }
-                }
+                };
                 send_to_constellation(sender, ConstellationMsg::AllowNavigationResponse(id, allow));
             }
             EmbedderMsg::GetClipboardContents(sender) => {
@@ -297,5 +291,19 @@ impl Window {
             }
         }
         false
+    }
+
+    fn allow_navigation(&self, url: ServoUrl) -> crate::Result<bool> {
+        if let Some(ref sender) = *self.event_listeners.on_navigation_starting.lock().unwrap() {
+            let (result_sender, receiver) =
+                ipc::channel::<bool>().map_err(|e| ipc::IpcError::Io(e))?;
+            sender
+                .send((url.into_url(), result_sender))
+                .map_err(|e| ipc::IpcError::Bincode(e))?;
+            let result = receiver.recv()?;
+            Ok(result)
+        } else {
+            Ok(true)
+        }
     }
 }
