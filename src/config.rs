fe58ec4745d9d2bc47e704b6a114d5f6c1dee549
Tony <legendmastertony@gmail.com>
2025-03-11 13:18:59 +0800

    Centralize window attributes to use the config

--- src/config.rs
+++ src/config.rs
@@ -312,7 +312,9 @@ impl Config {
             .user_agent
             .unwrap_or_else(|| default_user_agent_string().to_string());
 
-        let mut window_attributes = winit::window::Window::default_attributes();
+        let mut window_attributes = winit::window::Window::default_attributes()
+            .with_transparent(true)
+            .with_decorations(false);
         // set min inner size
         // should be at least able to show the whole control panel
         // FIXME: url input has weird behavior that will expand lager when having long text
